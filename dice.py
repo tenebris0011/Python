bc0d14cc4be1ac3d6ae4511bdfeb32537ba3ea14
tenebris0011 <tenebris0011@users.noreply.github.com>
2016-06-14 22:42:30 -0500

    Update dice.py
    Modified if and elif so that "You must role higher than a 10 to win." becomes true in the print.

--- dice.py
+++ dice.py
@@ -17,11 +17,11 @@ You must role higher than a 10 to win.
     dice_value=(random.randint(min,max))
     print ("You Rolled " + str(dice_value))
 
-    if dice_value >= 10:
+    if dice_value >= 11:
       print ('''Your staff begins to hum as you say your incantation.
 The demonic intitiy, begins to shreak with a blood curtling sound.
 You stand your ground, and banish it!''')
-    elif dice_value <= 10:
+    elif dice_value <= 11:
       print ('''You watch in dispair, as the intity devours your friends.
 You stand their, with no where to run, knowing that this is the end...''')
     roll=input('Roll Again?')
