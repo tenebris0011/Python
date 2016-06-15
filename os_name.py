98b16479f975b85ec05fcfb5f0b87e62a612fe11
tenebris0011 <tenebris0011@users.noreply.github.com>
2016-06-14 22:36:45 -0500

    Create os_name.py

---
+++ os_name.py
@@ -0,0 +1,13 @@
+import os
+
+name = os.getenv('COMPUTERNAME')
+
+answer = input('Would you like to know your computer\'s name? (yes/no)')
+
+if answer == 'yes':
+    print('Your computer\'s name is: ', name)
+elif answer == 'no':
+    print('Well, maybe some other time than.')
+
+else:
+    print('That is not a valid option.')
