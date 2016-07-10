6096b4e107afa1b4e271cd01165914087760aa99
tenebris0011 <tenebris0011@users.noreply.github.com>
2016-07-10 15:56:24 -0500

    Spelling
    Correcting spelling.

--- os_name.py
+++ os_name.py
@@ -7,6 +7,6 @@ answer = input('Would you like to know your computer\'s name? (yes/no)')
 if answer == 'yes':
     print('Your computer\'s name is: ', name)
 elif answer == 'no':
-    print('Well, maybe some other time than.')
+    print('Well, maybe some other time then.')
 else:
     print('That is not a valid option.')
