1ae46560fc057cbb436c03c736fe3dee2b51066d
tenebris0011 <tenebris0011@users.noreply.github.com>
2016-08-01 21:12:30 -0500

    Update password.py
    Modifying it to provide the ability to output to a text file with a name for what its for. 
    THIS IS NOT A SECURE WAY TO STORE YOUR PASSWORDS.
    THIS IS SIMPLY THIS WAY FOR IF YOU NEED TO CREATE MULTIPLE LOGINS IT IS RECOMMEND TO REMOVE THE TEXT FILE AND

--- password.py
+++ password.py
@@ -1,7 +1,32 @@
 import string
 import random
+import os
+import errno
 
-def id_generator(size=10, chars=string.ascii_uppercase + string.digits + string.ascii_lowercase):
+use = input('What is the password for?' ) #This is so that it labels what the generated numbers are for.
+flags = os.O_CREAT | os.O_EXCL | os.O_WRONLY
+def id_generator(size=10, chars=string.ascii_letters + string.digits):
         return ''.join(random.SystemRandom().choice(chars) for _ in range(size))
-    
-print (id_generator())
+
+id = id_generator()
+
+f = open('password.txt', 'a')
+
+f.write(use + "\n")
+f.write(id + "\n")
+f.write("\n")
+
+f.close()
+
+try:
+    file_handle = os.open('password.txt', flags)
+except OSError as e:
+    if e.errno == errno.EEXIST:
+        pass
+    else:
+        raise
+else:
+    with os.fdopen(file_handle, 'w') as file_obj:
+        file_obj.write(use + "\n")
+        file_obj.write(id + "\n")
+
